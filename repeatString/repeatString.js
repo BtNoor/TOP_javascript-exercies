9678cd7923dc1f463b7793d00ac1578bd02a09d3
Ben Noor <bennoor@Bens-MacBook-Pro.local>
2021-03-21 13:45:53 +1300

    Completed first two challenges

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    
+    //Variable to hold the returned value while iterating
+    let returnedString = ""
 
+    for (i = 0; i < num; i++) {
+        returnedString += word;
+    }
+
+    if (num < 0) {
+        returnedString = 'ERROR';
+    }
+
+    return returnedString;
 }
 
 module.exports = repeatString
