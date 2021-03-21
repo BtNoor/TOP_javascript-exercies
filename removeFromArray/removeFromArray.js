4c634b03bbad8a0d3639159a9b0c18904cb8dfb5
Ben Noor <bennoor@Bens-MacBook-Pro.local>
2021-03-21 14:23:34 +1300

    completed removeFrom tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(rem, params) {
 
+    let newArray = rem;
+
+    for (c = 1; c < arguments.length; c++) {
+        for (i = 0; i < newArray.length; i++) {
+            if (arguments[c] === newArray[i]) {
+                newArray.splice(i, 1);
+            }
+        }
+    }
+    
+    return newArray;
 }
 
 module.exports = removeFromArray
