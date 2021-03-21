9a09165be9d97e2aaa345cf2475bdfdf709433a0
Ben Noor <bennoor@Bens-MacBook-Pro.local>
2021-03-21 14:42:07 +1300

    Completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    let totalSum = 0;
+
+    if (a <= 0 || typeof a != 'number' || typeof b != 'number') {
+        return 'ERROR'
+    } else if (a < b) {
+        for (i = 0; i <= b; i++) {
+            if (i >= a) {
+                totalSum += i;
+            }
+        }
+    } else if (a > b) {
+        for (i = 0; i <= a; i++) {
+            if (i >= b) {
+                totalSum += i;
+            }
+        }
+    }
+
+
+    return totalSum;
 }
 
 module.exports = sumAll
