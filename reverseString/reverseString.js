cae81f123f7db01f7faa0a7a715cbf38a63c7aac
Ben Noor <bennoor@Bens-MacBook-Pro.local>
2021-03-21 13:57:46 +1300

    Completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    //Variable to iterate string to
+    let storedString = []
+
+    for (i = 0; i < word.length; i++) {
+        storedString.unshift(word[i]);
+        
+    }
+    storedString = storedString.join("")
+    //storedString = storedString.toString();
+
+    return storedString;
 }
 
 module.exports = reverseString
